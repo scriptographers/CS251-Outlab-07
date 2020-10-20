dc8edd42820d4da4b8513b2907f9272e5e539daa
devansh-dvj <devansh.dvj@gmail.com>
2020-10-20 22:51:27 +0530

    Merged hitman_1 and hitman

--- Q1/hitman.py
+++ Q1/hitman.py
@@ -6,30 +6,27 @@ try:
     conn = sq.connect(DB_NAME)
     cur = conn.cursor()
 
-    SMQ = """
-        SELECT striker, player_name, COUNT(*) AS total,
-        COUNT(CASE WHEN runs_scored=6 THEN 1 END) AS sixes,
-        COUNT(CASE WHEN runs_scored=6 THEN 1 END)*1.0/COUNT(*) AS avg
+    HITMAN = """
+        SELECT striker, player_name,
+        SUM(CASE WHEN runs_scored = 6 THEN 1 ELSE 0 END) AS n_sixes,
+        COUNT(ball_id) AS n_balls
         FROM BALL_BY_BALL
         INNER JOIN PLAYER ON BALL_BY_BALL.striker = PLAYER.player_id
-        GROUP BY player_name
-        ORDER BY avg DESC
-   """
+        GROUP BY striker
+        ORDER BY (n_sixes*1.0/n_balls) DESC
+    """
 
-    cur.execute(SMQ)
+    cur.execute(HITMAN)
     results = cur.fetchall()
 
-    # print(results)
     for r in results:
-        print("{},{},{},{},{}".format(r[0], r[1], r[2], r[3], r[4]))
+        print("{},{},{},{},{}".format(r[0], r[1], r[2], r[3], r[2] / r[3]))  # "Hack"
 
     cur.close()
-
 
 # Error handling
 except sq.Error as error:
     print("Errors: ", error)
-
 
 # Close the connection
 finally:
