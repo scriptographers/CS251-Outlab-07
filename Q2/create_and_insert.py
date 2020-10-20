b409e5d35034bff05a85fab26cc38d42dcfe78b9
devansh-dvj <devansh.dvj@gmail.com>
2020-10-21 02:29:20 +0530

    Q2: Task 1: Completed

---
+++ Q2/create_and_insert.py
@@ -0,0 +1,33 @@
+import sqlite3 as sql
+
+crt = open('University Schema', 'r').read()
+ins = open('smallRelationsInsertFile.sql', 'r').read()
+DB_NAME = "univ.db"
+
+conn = sql.connect(DB_NAME)
+cur = conn.cursor()
+
+for x in crt.split(';') + ins.split(';'):
+    try:
+        cur.execute(x)
+        conn.commit()
+    except sql.Error as error:
+        continue
+
+xx = ['prereq',
+      'time_slot',
+      'advisor',
+      'takes',
+      'student',
+      'teaches',
+      'section',
+      'instructor',
+      'course',
+      'department',
+      'classroom']
+for i in xx:
+    cur.execute("SELECT * FROM {}".format(i))
+    print(cur.fetchall())
+
+if (conn):
+    conn.close()
