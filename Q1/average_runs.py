e799e82062604b8c87539214308afcb681d649b8
Harshit Varma <varmaharshit2@gmail.com>
2020-10-20 22:35:19 +0530

    Q1: Task 3: Shortened code by a large amount
    Using only SQL now

--- Q1/average_runs.py
+++ Q1/average_runs.py
@@ -7,46 +7,26 @@ try:
     conn = sq.connect(DB_NAME)
     cur  = conn.cursor()
 
-    SMQ = "SELECT match_id, venue_name FROM MATCH WHERE venue_name is NOT NULL"
-
-    cur.execute(SMQ)
+    AVG = """
+        SELECT venue_name, 
+        SUM(runs_scored) AS total_runs,
+        COUNT(DISTINCT MATCH.match_id) AS n_matches
+        FROM BALL_BY_BALL
+        INNER JOIN MATCH ON BALL_BY_BALL.match_id =MATCH.match_id
+        GROUP BY venue_name
+        ORDER BY (total_runs*1.0/n_matches) DESC
+    """
+    cur.execute(AVG)
     results = cur.fetchall()
 
-    VENUE_RUNS = { venue:0 for _, venue in results }
-    VENUE_N_MATCHES = { venue:0 for _, venue in results }
+    for r in results:
+        print("{},{}".format(r[0], r[1]/r[2])) # "Hack"
 
-    for m_id, venue in results:
-
-        SBBQ = "SELECT SUM(runs_scored) FROM BALL_BY_BALL WHERE match_id=?"
-        cur.execute(SBBQ, (m_id,))
-        total_runs_scored = cur.fetchall()[0][0]
-
-        if (total_runs_scored != None):
-            VENUE_N_MATCHES[venue] += 1
-            VENUE_RUNS[venue] += total_runs_scored
-        # else:
-        #    print(m_id)
-
-    VENUE_AVG = { 
-        venue: (VENUE_RUNS[venue]/VENUE_N_MATCHES[venue])
-        for _,venue in results if (VENUE_N_MATCHES[venue] != 0) 
-    }
-
-    VENUE_AVG = { 
-        venue: avg 
-        for venue,avg in sorted(VENUE_AVG.items(), key=lambda item:item[1], reverse=True) 
-    }
-
-    for venue, avg in VENUE_AVG.items():
-        print("{},{}".format(venue, avg))
-    
     cur.close()
-
 
 # Error handling
 except sq.Error as error:
     print("Errors: ", error)
-
 
 # Close the connection
 finally:
