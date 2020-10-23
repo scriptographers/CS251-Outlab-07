3a76d36c5f7332ef884807d01575d5926db7a5dc
Harshit Varma <varmaharshit2@gmail.com>
2020-10-23 22:55:25 +0530

    Q1: Task-6: Updated according to new PS

--- Q1/make_points_table.py
+++ Q1/make_points_table.py
@@ -1,8 +1,6 @@
-import csv
 import sqlite3 as sq
 
 DB_NAME = "ipl.db"
-FILE_PATH   = "match.csv"
 
 try:
     conn = sq.connect(DB_NAME)
@@ -15,7 +13,7 @@ try:
     CREATE = """
         CREATE TABLE IF NOT EXISTS POINTS_TABLE 
         (
-            team_id INTEGER,
+            team_id INTEGER PRIMARY KEY, 
             team_name TEXT,
             points INTEGER DEFAULT 0,
             nrr DECIMAL DEFAULT 0
@@ -31,41 +29,47 @@ try:
     TEAM_POINTS = {team[0]:0 for team in teams}
     TEAM_NRR = {team[0]:0 for team in teams}
 
-    with open(FILE_PATH) as f:
-        rows   = csv.reader(f)
-        header = next(rows)
+    GET_DATA = "SELECT * FROM MATCH"
+    cur.execute(GET_DATA)
+    rows = cur.fetchall()
 
-        for row in rows:
+    for row in rows:
 
-            team1 = int(row[2])
-            team2 = int(row[3])
-            winner = row[9]
-            margin = row[-1]
-            win_type = row[12]
+        team1 = int(row[2])
+        team2 = int(row[3])
+        winner = row[9]
+        margin = row[-1]
+        win_type = row[12]
 
+        if win_type.lower() != "tie":
             if winner != "NULL":
                 winner = int(winner)
                 TEAM_POINTS[winner] += 2
             else:
+                # Match abandoned
                 TEAM_POINTS[team1] += 1
                 TEAM_POINTS[team2] += 1
+        else:
+            TEAM_POINTS[team1] += 1
+            TEAM_POINTS[team2] += 1
 
-            nrr = 0
-            if margin != "NULL":
-                margin = int(margin)
-                if win_type == "runs":
-                    nrr = margin/20
-                elif win_type == "wickets":
-                    nrr = margin/10
-                else:
-                    nrr = 0
+        nrr = 0
+        if margin != "NULL":
+            margin = int(margin)
+            if win_type == "runs":
+                nrr = margin/20
+            elif win_type == "wickets":
+                nrr = margin/10
+            else:
+                nrr = 0
 
-            if team1 == winner:
-                TEAM_NRR[team1] += nrr
-                TEAM_NRR[team2] -= nrr
-            elif team2 == winner:
-                TEAM_NRR[team1] -= nrr
-                TEAM_NRR[team2] += nrr
+        # NRR changes only when there is a winner
+        if team1 == winner:
+            TEAM_NRR[team1] += nrr
+            TEAM_NRR[team2] -= nrr
+        elif team2 == winner:
+            TEAM_NRR[team1] -= nrr
+            TEAM_NRR[team2] += nrr
 
     DATA = []
     for team in teams: 
@@ -76,9 +80,8 @@ try:
     cur.executemany(ADD_DATA, DATA)
     conn.commit()
 
-    # CHECK = "SELECT * FROM POINTS_TABLE"
-    # cur.execute(CHECK)
-    # print(cur.fetchall())
+    for i in range(len(DATA)):
+        print("{},{},{},{}".format(i+1, DATA[i][1], DATA[i][2], DATA[i][3]))
 
     cur.close()
 
