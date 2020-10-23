d218b7be50535a528c2c4dc1faac696931427449
Harshit Varma <varmaharshit2@gmail.com>
2020-10-23 22:24:14 +0530

    Q1: Task 4: Incorporated new PS changes

--- Q1/max_runs.py
+++ Q1/max_runs.py
@@ -10,11 +10,11 @@ try:
         SELECT striker, player_name, SUM(runs_scored) as total_runs
         FROM BALL_BY_BALL
         INNER JOIN PLAYER ON BALL_BY_BALL.striker = PLAYER.player_id
-        GROUP BY striker, player_name
-        ORDER BY total_runs DESC
+        GROUP BY striker
+        ORDER BY total_runs DESC, player_name ASC
         LIMIT 20;
     """
-
+    # The CSV counts the extra runs separately, and they aren't included in the runs_scored
     cur.execute(ORANGE_CAP)
     results = cur.fetchall()
 
@@ -23,11 +23,9 @@ try:
 
     cur.close()
 
-
 # Error handling
 except sq.Error as error:
     print("Errors: ", error)
-
 
 # Close the connection
 finally:
